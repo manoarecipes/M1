18c74bf88a44221ba66243b3661c82092a793801
mserai <mserai@hawaii.edu>
2017-11-16 14:03:37 -1000

    fixed import

--- app/server/main.js
+++ app/server/main.js
@@ -2,5 +2,5 @@ import '/imports/startup/server';
 import '/imports/api/base';
 import '/imports/api/profile';
 import '/imports/api/interest';
-import '/imports/api/ingredient';
+import '/imports/api/ingredients';
 
