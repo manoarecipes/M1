45206b5afa63b5d3c3e8f38d49e2d4a29fe6457d
David Herman <DavidCHerman@users.noreply.github.com>
2017-11-19 23:56:33 -1000

    started add-recipe-page.js
    Awaiting recipe collection

---
+++ app/imports/ui/pages/user/add-recipe-page.js
@@ -0,0 +1,84 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Ingredients } from '/imports/api/ingredients/IngredientsCollection';
+import { Recipes } from '/import/api/recipes/RecipesCollection';
+
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
+
+Template.Add_Recipe_Page.onCreated(function onCreated() {
+  this.subscribe(Ingredients.getPublicationName());
+  this.subscribe(Profiles.getPublicationName());
+  this.subscribe(Recipes.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displaySuccessMessage, false);
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = Recipes.getSchema().namedContext('Add_Recipe_Page');
+});
+
+Template.Add_Recipe_Page.helpers({
+  successClass() {
+    return Template.instance().messageFlags.get(displaySuccessMessage) ? 'success' : '';
+  },
+  displaySuccessMessage() {
+    return Template.instance().messageFlags.get(displaySuccessMessage);
+  },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  profile() {
+    return Profiles.findDoc(FlowRouter.getParam('username'));
+  },
+  recipe() {
+    return Recipes.findDoc(FlowRouter.getParam('recipeName'));
+  },
+  ingredients() {
+    const recipe = Recipes.findDoc(FlowRouter.getParam('recipeName'));
+    const selectedIngredients = recipe.ingredients;
+    return recipe && _.map(Ingredients.findAll(),
+        function makeIngredientObject(ingredient) {
+          return { label: ingredient.name, selected: _.contains(selectedIngredients, ingredient.name) };
+        });
+  },
+});
+
+
+Template.Add_Recipe_Page.events({
+  'submit .profile-data-form'(event, instance) {
+    event.preventDefault();
+    const recipeName = event.target.recipe.value;
+    const description = event.target.description.value;
+    const username = FlowRouter.getParam('username'); // schema requires username.
+    const instructions = event.target.instructions.value;
+    const picture = event.target.picture.value;
+    const selectedFoodTags = _.filter(event.target.foodTags.selectedOptions, (option) => option.selected);
+    const foodTags = _.map(selectedFoodTags, (option) => option.value);
+    const selectedIngredients = _.filter(event.target.Ingredients.selectedOptions, (option) => option.selected);
+    const ingredients = _.map(selectedIngredients, (option) => option.value);
+
+    const updatedRecipeData = { recipeName, description, username, instructions, picture, foodTags, ingredients };
+
+    // Clear out any old validation errors.
+    instance.context.reset();
+    // Invoke clean so that updatedProfileData reflects what will be inserted.
+    const cleanData = Recipes.getSchema().clean(updatedRecipeData);
+    // Determine validity.
+    instance.context.validate(cleanData);
+
+    if (instance.context.isValid()) {
+      const docID = Recipes.findDoc(FlowRouter.getParam('username'))._id;
+      const id = Recipes.update(docID, { $set: cleanData });
+      instance.messageFlags.set(displaySuccessMessage, id);
+      instance.messageFlags.set(displayErrorMessages, false);
+    } else {
+      instance.messageFlags.set(displaySuccessMessage, false);
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+});
+
+
+
