86b5bd2e13375c5af33888f4b275c69adeb11976
David Herman <DavidCHerman@users.noreply.github.com>
2017-12-05 00:46:11 -1000

    myRecipes functional need favRecipes

--- app/imports/ui/components/user/profile.recipe.js
+++ app/imports/ui/components/user/profile.recipe.js
@@ -1,6 +1,11 @@
 import { Template } from 'meteor/templating';
 import { Recipes } from '/imports/api/recipe/RecipeCollection';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
-Template.Directory_Page.onCreated(function onCreated() {
+Template.Profile_Recipe.onCreated(function onCreated() {
   this.subscribe(Recipes.getPublicationName());
+});
+
+Template.Profile_Recipe.helpers({
+
 });
--- app/imports/ui/pages/user/profile-landing.js
+++ app/imports/ui/pages/user/profile-landing.js
@@ -5,6 +5,7 @@ import { Recipes } from '/imports/api/recipe/RecipeCollection';
 
 Template.Profile_Landing.onCreated(function onCreated() {
   this.subscribe(Profiles.getPublicationName());
+  this.subscribe(Recipes.getPublicationName());
   this.context = Profiles.getSchema().namedContext('Profile_Page');
 });
 
@@ -16,7 +17,7 @@ Template.Profile_Landing.helpers({
     return Profiles.findDoc(FlowRouter.getParam('username'));
   },
   myRecipes() {
-    console.log(Recipes.dumpAll({ username: FlowRouter.getParam('username') }));
-    return Recipes.dumpAll({ username: FlowRouter.getParam('username') });
+    console.log(Recipes.find({ username: FlowRouter.getParam('username') }).map(x => x));
+    return Recipes.find({ username: FlowRouter.getParam('username') }).map(x => x);
   },
 });
