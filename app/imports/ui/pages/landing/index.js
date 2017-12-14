c7e35e9d3ea7af8ea8544a1d3075310727d41cf2
mserai <mserai@hawaii.edu>
2017-12-13 16:42:55 -1000

    update

--- app/imports/ui/pages/landing/index.js
+++ app/imports/ui/pages/landing/index.js
@@ -3,6 +3,3 @@ import './landing-page.css';
 import './mr-landing-page.html';
 import './mr-landing-page.css';
 import './mr-landing-page.js';
-import './filter-ingredients.html';
-import './filter-ingredients-directory.html';
-import './filter-ingredients.js';
